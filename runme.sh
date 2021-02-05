4dc23fc74b80c106babf63f92c79b87bc7d9b457
Björn Höfling <bjoern.hoefling@camptocamp.com>
2021-02-05 12:21:31 +0100

    First example.

---
+++ runme.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Usage ./runexample example01 [--debug]
+# Will example and print out as pdf/png
+
+EXAMPLE=$1
+DEBUG=""
+if [ "$2" == "--debug" ]; then
+    DEBUG="--debug-jvm"
+fi
+
+if [ -z $EXAMPLE]; then
+    echo "Usage: ./runexample example01 [--debug]"
+    exit 1
+fi
+ 
+
+RUN_PATH="$(pwd)/$EXAMPLE"
+MFP_PATH=$HOME/dev/mfp/mapfish-print
+
+echo "DEBUG=$DEBUG"
+echo "RUN_PATH: $RUN_PATH"
+echo "MFP_PATH=$MFP_PATH"
+
+cd $RUN_PATH
+$MFP_PATH/gradlew print $DEBUG -PprintArgs="-config $RUN_PATH/config.yaml -spec $RUN_PATH/requestData.json -output $RUN_PATH/output"
